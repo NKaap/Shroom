3db0e38896127dfd282c6cb894c650c1f4748cdf
Sjoerd21 <78026651+Sjoerd21@users.noreply.github.com>
2021-09-05 20:25:56 +0200

    sjoerd main menu
    main menu concept

---
+++ Shroom/Shroom/Assets/Scripts/sjoerd/camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class camera : MonoBehaviour
+{
+    public Transform cam;
+    public float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
