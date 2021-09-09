b3ac2aecef0395c567d1b771f9c085a2538b56ec
arthur-lava <97080076@st.deltion.nl>
2021-09-09 16:01:25 +0200

    epische health barr
    das nii barky firend das nii barky

---
+++ Shroom/Shroom/Assets/Scripts/MainShroom.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainShroom : MonoBehaviour
+{
+    public float MaxHealth = 100;
+    public float CurrentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
